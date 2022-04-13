54b2cf0b466a75c1ad82c8957146e6321d99127e
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-04-13 21:16:43 +0500

    Update Dev-Activate.ts

--- src/commands/Dev/Dev-Activate.ts
+++ src/commands/Dev/Dev-Activate.ts
@@ -6,11 +6,11 @@ import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
   constructor(client: WAClient, handler: MessageHandler) {
     super(client, handler, {
-      adminOnly: true,
-      command: "activate",
-      aliases: ["act"],
+      modsOnly: true,
+      command: "d-activate",
+      aliases: ["d-act"],
       description: "activate certain features on group-chats",
-      category: "moderation",
+      category: "dev",
       usage: `${client.config.prefix}activate [events | mod | safe | nsfw | cmd | invitelink]`,
       baseXp: 0,
     });
